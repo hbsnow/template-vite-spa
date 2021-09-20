f98647a2efd20151da0fbe98cad0f372dbd96af4
hbsnow <yuki.4uing@gmail.com>
2021-09-20 16:22:50 +0900

    fix: add return type

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Button } from "./components/Button";
 import { useCount } from "./hooks/useCount";
 
-export const App = () => {
+export const App = (): JSX.Element => {
   const [count, { countUp, countDown }] = useCount();
 
   return (
